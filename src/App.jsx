60d2a79b73fb5b203bbabffbac7c924ff84e65b7
IlyassLho <ilyasslhouari@gmail.com>
2026-01-29 16:37:20 +0100

    Add routing for Analytics component in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import { Routes, Route } from 'react-router-dom';
 import Header from '../src/ilyas/Header'
+import Analytics from '../src/ilyas/Analytics'
 
 const App = () => {
   return (
     <div>
       <Header />
+
+      <Routes>
+        <Route path="/analysis" element={<Analytics />} />
+      </Routes>
     </div>
   );
 }
